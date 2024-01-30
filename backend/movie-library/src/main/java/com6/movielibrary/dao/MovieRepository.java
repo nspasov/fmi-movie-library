896f5dad7e30606129d7f5c07a0f737a2c4278e1
Nikolay Spasov <nspasov22@gmail.com>
2024-01-30 17:49:46 +0200

    backend api adjusted to accept search by title params

--- backend/movie-library/src/main/java/com6/movielibrary/dao/MovieRepository.java
+++ backend/movie-library/src/main/java/com6/movielibrary/dao/MovieRepository.java
@@ -2,8 +2,12 @@ package com6.movielibrary.dao;
 
 import com6.movielibrary.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 // creates REST routes
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-
+    Page<Movie> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }

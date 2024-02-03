fee51d7d7110f47a043a330e23df172331243f8f
Nikolay Spasov <nspasov22@gmail.com>
2024-02-03 17:41:05 +0200

    finished messages components and logic on frontend

--- frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -17,7 +17,7 @@ export default function LibraryServices(){
                     </p>
                     <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
                     {authState?.isAuthenticated ? 
-                                <Link type="button" className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white" to='#'>Services</Link>
+                                <Link type="button" className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white" to='/messages'>Services</Link>
                                 :
                                 <Link className="btn main-color btn-lg text-white" to="/login">Login</Link>
                             }
--- frontend/react-library/src/layouts/MessagesPage/MessagesPage.tsx
+++ frontend/react-library/src/layouts/MessagesPage/MessagesPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { PostNewMessage } from "./components/PostNewMessage";
+import { Messages } from "./components/Messages";
 
 export const MessagesPage = () => {
 
@@ -24,7 +25,7 @@ export const MessagesPage = () => {
                             <PostNewMessage/>
                         </div>
                         <div className="tab-pane fade" id="nav-message" role="tabpanel" aria-labelledby="nav-message-tab">
-                            {messagesClick ? <p>Messages</p> : <></>}
+                            {messagesClick ? <Messages/> : <></>}
                         </div>
                     </div>
                 </div>

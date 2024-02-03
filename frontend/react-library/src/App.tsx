df159cc204e5732b627895762cc00c058f81c477
Nikolay Spasov <nspasov22@gmail.com>
2024-02-03 16:24:44 +0200

    added messages page component to frontend

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -12,6 +12,7 @@ import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/MovieCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
+import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -51,6 +52,7 @@ export default function App() {
             <Route path='/login' render={() => <LoginWidget config={oktaConfig} />}/>
             <Route path='/login/callback' component={LoginCallback} />
             <SecureRoute path="/shelf"><ShelfPage/></SecureRoute>
+            <SecureRoute path="/messages"><MessagesPage/></SecureRoute>
           </Switch>
         </div>
         <Footer />
---
+++ frontend/react-library/src/layouts/MessagesPage/MessagesPage.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const MessagesPage = () => {
+
+    const [messagesClick, setMessagesClick] = useState(false);
+
+    return(
+        <>
+            <div className="container">
+                <div className="mt-3 mb-2">
+                    <nav>
+                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button onClick={() => setMessagesClick(false)} className="nav-link active" id="nav-send-message-tab" data-bs-toggle='tab' data-bs-target="#nav-send-message" type="button" role="tab" aria-controls="nav-send-message" aria-selected="true">
+                                Submit Message
+                            </button>
+                            <button onClick={() => setMessagesClick(true)} className="nav-link" id="nav-message-tab" data-bs-toggle='tab' data-bs-target="#nav-message" type="button" role="tab" aria-controls="nav-message" aria-selected="false">
+                                Admin Response/Pending
+                            </button>
+                        </div>
+                    </nav>
+                    <div className="tab-content" id="nav-tabContent">
+                        <div className="tab-pane fade show active" id="nav-send-message" role="tabpanel" aria-labelledby="nav-send-message-tab">
+                            <p>Post new Message</p>
+                        </div>
+                        <div className="tab-pane fade" id="nav-message" role="tabpanel" aria-labelledby="nav-message-tab">
+                            {messagesClick ? <p>Messages</p> : <></>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
+}
